e60844d7d5facbae41c7e08bc266557f3e145e6d
Luis Mansilla <l_mansilla92@yahoo.com>
2023-08-23 23:14:47 -0500

    added empty choices array, added if statements to the optionPrompts function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,8 @@ var passwordLength = 8;
 var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 var numberArray = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var specialChar = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '_', '{', '}', '~']
+var specialChar = ['!', '"', '#', '$', '%', '&', '(', ')', '*', '+', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '_', '{', '}', '~'];
+var choicesMade = [];
 
 // STEP 1: Grabs the button in html with generate ID and stores it in the JS variable named generateBtn 
 var generateBtn = document.querySelector("#generate");
@@ -12,9 +13,9 @@ var generateBtn = document.querySelector("#generate");
 // STEP 3: This function starts after button is clicked
 // Function writes a new password to the #password input
 function writePassword() {
+
   // Including this function inside writePassword function ensures the user is prompted with options when the button is clicked
   optionPrompts();
-
 
   var password = generatePassword();
 
@@ -31,7 +32,24 @@ function generatePassword(){
 
 // This function prompts the user with options for the new password criteria
 function optionPrompts(){
-  passwordLength = parseInt(prompt("How long would you like your password to be? \n (Must be between 8 - 128 characters)"))
+  passwordLength = parseInt(prompt("How long would you like your password to be? \n (Must be between 8 - 128 characters)"));
+
+  // Adds if statement to alert user if they didn't enter valid inputs
+  // isNAN(passwordLength) checks if the user entered a number or not
+  if(isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
+    alert("Please make sure you entered a valid number, and the password length is between 8 - 128 characters");
+    // Triggers optionsPrompt again if user entered invalid options
+    optionPrompts();
+  } if(confirm("Do you want to include lowercase letters?")){
+    // Updates empty choicesMade array value to include lowercase letters by concatinating both values
+    choicesMade = choicesMade.concat(lowerCase);
+  } if(confirm("How about uppercase letters?")){
+    choicesMade = choicesMade.concat(upperCase);
+  } if(confirm("Would you like to include numbers")){
+    choicesMade = choicesMade.concat(numberArray);
+  } if(confirm("Lastly do you want special characters in your new password?")){
+    choicesMade = choicesMade.concat(specialChar);
+  } return true;
 }
 
 
